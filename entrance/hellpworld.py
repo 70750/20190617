9e0278daae5235932cd649af2f30ccbf96ca1b19
70750 <cnofne1978@163.com>
2020-06-25 19:23:55 +0800

    hello world

---
+++ entrance/hellpworld.py
@@ -0,0 +1,14 @@
+print("hello world")
+print("Go big or go home")
+print("10.24(程序员之日)寓意：\n")
+print("  低调，踏实，核心\n")
+print(" 1024M=1G,谐音一级棒")
+
+print("    丽江市新闻采编系统\n")
+print("用户名：_______\n")
+print("密码：_________\n")
+print("验证码：_______ 3+5=?")
+
+print("###################")
+print("笑        飞")
+print("书        雪\n")
